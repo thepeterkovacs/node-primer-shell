707a6b230adaa0fb2adbc8947c98edad696f6def
thepeterkovacs <peterkov37@gmail.com>
2023-10-09 13:31:40 +0200

    service install event handlers and change name

--- src/service/core.ts
+++ src/service/core.ts
@@ -2,8 +2,20 @@ import { Service, ServiceConfig } from "node-windows"
 import path from "path"
 
 const config: ServiceConfig = {
-	name: "node-primer-shell-service",
+	name: "Node Primer Shell Service",
 	script: path.join(__dirname, "server.js"),
 }
 
 export const service = new Service(config)
+
+service.on("install", function () {
+	console.log("Service installed successfully")
+})
+
+service.on("alreadyinstalled", function () {
+	console.log("Service already installed")
+})
+
+service.on("invalidinstallation", function () {
+	console.log("Invalid installation")
+})
