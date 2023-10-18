5889158013cebd6a7b355bf042ac5e70049061f5
thepeterkovacs <peterkov37@gmail.com>
2023-10-18 13:50:51 +0200

    remove express from app

--- src/app.ts
+++ src/app.ts
@@ -1,17 +1,5 @@
-import * as dotenv from "dotenv"
-import express from "express"
+function main() {
+	console.log("Node Primer Shell")
+}
 
-dotenv.config({
-	path: ".env.local",
-})
-
-const app = express()
-const port = process.env.PORT
-
-app.get("/", (_, res) => {
-	res.status(200).send("Node Primer Shell")
-})
-
-app.listen(port, () => {
-	console.log(`Node Primer Shell listening on port ${port}`)
-})
+main()
